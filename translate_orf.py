ddf155be8dfb480dfd9b7a7b25467c9f2422ceb2
Fritzner Pierre <fritznerp@outlook.com>
2026-04-04 21:54:01 -0500

    Started with the list of amino acids

--- translate_orf.py
+++ translate_orf.py
@@ -11,7 +11,28 @@ Usage:
 
 Dependencies:
     - find_orf.py: must contain find_first_orf and parse_sequence_from_path functions
-    - translate.py: must contain translate_sequence function and genetic code dictionary
 """
 
+import argparse
+from find_orf import find_first_orf, parse_sequence_from_path
 
+
+def translate_first_orf(sequence,
+        start_codons=['AUG'],
+        stop_codons=['UAA', 'UAG', 'UGA'],
+        genetic_code={
+            'GUC': 'Valine', 'ACC': 'Threonine', 'GUA': 'Valine', 'GUG': 'Valine', 'ACU': 'Threonine',
+            'AAC': 'Asparagine', 'CCU': 'Proline', 'UGG': 'Tryptophan', 'AGC': 'Serine', 'AUC': 'Isoleucine',
+            'CAU': 'Histidine', 'AAU': 'Asparagine', 'AGU': 'Serine', 'GUU': 'Valine', 'CAC': 'Histidine',
+            'ACG': 'Threonine', 'CCG': 'Proline', 'CCA': 'Proline', 'ACA': 'Threonine', 'CCC': 'Proline',
+            'UGU': 'Cysteine', 'GGU': 'Glycine', 'UCU': 'Serine', 'GCG': 'Alanine', 'UGC': 'Cysteine',
+            'CAG': 'Glutamine', 'GAU': 'Aspartic acid', 'UAU': 'Tyrosine', 'CGG': 'Arginine', 'UCG': 'Serine',
+            'AGG': 'Arginine', 'GGG': 'Glycine', 'UCC': 'Serine', 'UCA': 'Serine', 'UAA': 'Stop',
+            'GGA': 'Glycine', 'UAC': 'Tyrosine', 'GAC': 'Aspartic acid', 'UAG': 'Stop', 'AUA': 'Isoleucine',
+            'GCA': 'Alanine', 'CUU': 'Leucine', 'GGC': 'Glycine', 'AUG': 'Methionine', 'CUG': 'Leucine',
+            'GAG': 'Glutamic acid', 'CUC': 'Leucine', 'AGA': 'Arginine', 'CUA': 'Leucine', 'GCC': 'Alanine',
+            'AAA': 'Lysine', 'AAG': 'Lysine', 'CAA': 'Glutamine', 'UUU': 'Phenylalanine', 'CGU': 'Arginine',
+            'CGC': 'Arginine', 'CGA': 'Arginine', 'GCU': 'Alanine', 'GAA': 'Glutamic acid', 'AUU': 'Isoleucine',
+            'UUG': 'Leucine', 'UUA': 'Leucine', 'UGA': 'Stop', 'UUC': 'Phenylalanine'
+        },
+        ):
