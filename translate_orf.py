5373be008b15c763ed2c58ea722ba574a0321ac1
Fritzner Pierre <fritznerp@outlook.com>
2026-04-04 21:56:44 -0500

    Added script to find the first ORF and translate it into an amino acid sequence.

--- translate_orf.py
+++ translate_orf.py
@@ -36,3 +36,29 @@ def translate_first_orf(sequence,
             'UUG': 'Leucine', 'UUA': 'Leucine', 'UGA': 'Stop', 'UUC': 'Phenylalanine'
         },
         ):
+
+"""
+    Find the first ORF and translate it into an amino acid sequence.
+"""
+
+    orf = find_first_orf(sequence, start_codons, stop_codons)
+
+    if not orf:
+        return None
+
+    amino_acids = []
+
+    for i in range(0, len(orf), 3):
+        codon = orf[i:i+3]
+
+        if len(codon) < 3:
+            break
+
+        aa = genetic_code.get(codon, "Unknown")
+
+        if aa == "Stop":
+            break
+
+        amino_acids.append(aa)
+
+    return amino_acids
